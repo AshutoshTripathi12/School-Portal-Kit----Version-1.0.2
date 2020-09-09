d2f69b6291aa2c14d04a7daf5fe9149e50e73452
AshutoshTripathi12 <70275501+AshutoshTripathi12@users.noreply.github.com>
2020-09-10 01:58:38 +0530

    Update principal_dashboard.php

--- resources/view/principal_dashboard.php
+++ resources/view/principal_dashboard.php
@@ -247,8 +247,7 @@
               <!-- User Menu-->
               <li class='dropdown'><a class='dropdown-toggle' href='#' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><i class='fa fa-user fa-lg'></i></a>
                 <ul class='dropdown-menu settings-menu'>
-                  <li><a href='page-user.html'><i class='fa fa-cog fa-lg'></i> Settings</a></li>
-                  <li><a href='page-user.html'><i class='fa fa-user fa-lg'></i> Profile</a></li>
+
                   <li><a href='principal_dashboard.php?logout'><i class='fa fa-sign-out fa-lg'></i> Logout</a></li>
                 </ul>
               </li>
