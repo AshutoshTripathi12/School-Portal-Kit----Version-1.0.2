a47d287f1a84b608e7eaa0aa634d7c0edff26969
AshutoshTripathi12 <70275501+AshutoshTripathi12@users.noreply.github.com>
2020-09-09 22:50:49 +0530

    Update accountant_dashboard.php

--- resources/view/accountant_dashboard.php
+++ resources/view/accountant_dashboard.php
@@ -136,8 +136,8 @@
               <!-- User Menu-->
               <li class='dropdown'><a class='dropdown-toggle' href='#' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><i class='fa fa-user fa-lg'></i></a>
                 <ul class='dropdown-menu settings-menu'>
-                  <li><a href='page-user.html'><i class='fa fa-cog fa-lg'></i> Settings</a></li>
-                  <li><a href='page-user.html'><i class='fa fa-user fa-lg'></i> Profile</a></li>
+                  <li><a href='accountant_dashboard.php'><i class='fa fa-cog fa-lg'></i> Settings</a></li>
+                  <li><a href='accountant_profile.php'><i class='fa fa-user fa-lg'></i> Profile</a></li>
                   <li><a href='accountant_dashboard.php?logout'><i class='fa fa-sign-out fa-lg'></i> Logout</a></li>
                 </ul>
               </li>
